c5ae3b238426aa1b0af8bc82e8e0381c9edca24f
Aaron Williams <amwill19@ncsu.edu>
2017-02-14 22:07:26 -0500

    Badly needed style fix in pickleDB.py: added a space after every single darned comma

--- database/pickleDB.py
+++ database/pickleDB.py
@@ -26,7 +26,7 @@ def _updateDBFormat(database):
         database['version'] = _CURRENT_DB_VERSION
 
 class Database:
-    def __init__(self,fileName,pickle):
+    def __init__(self, fileName, pickle):
         self.fileName=fileName
         self.pickle=pickle
         try:
@@ -49,33 +49,33 @@ class Database:
         print(repr(serverId))
         return self.db[serverId]
 
-    def createGuild(self,guild):
+    def createGuild(self, guild):
         if self.guildExists(guild):
             raise ValueError(guild.name + 'is already known to Battlebot!')
         else:
-            self.db[guild.id]=Battle(guild)#makeBattle(guild.id,Battle(guild))
+            self.db[guild.id]=Battle(guild)#makeBattle(guild.id, Battle(guild))
 
-    def guildExists(self,guild):
+    def guildExists(self, guild):
         return guild in self.db
 
-    def getAllCharacters(self,serverId):
+    def getAllCharacters(self, serverId):
         return self.db[serverId].characters
 
     def getCharacter(self, serverId, charName):
         battle = self.getBattle(serverId)
         return battle.characters[charName.lower()]
 
-    def insertCharacter(self,server,char):
+    def insertCharacter(self, server, char):
         if not self.guildExists(server):
             self.createGuild(server)
         self.db[server.id].addCharacter(char)
 
-    def toggleSecretChar(self,serverId,charName):
-        char = self.getCharacter(serverId,charName)
+    def toggleSecretChar(self, serverId, charName):
+        char = self.getCharacter(serverId, charName)
         char.secret = not char.secret
         return char
 
-    def updateStats(self,serverId,charName,stats,overwriteBattleLock=False):
+    def updateStats(self, serverId, charName, stats, overwriteBattleLock=False):
         battle = self.getBattle(serverId)
         char = self.getCharacter(charName)
         if char not in battle.participants or isGM:
@@ -83,19 +83,19 @@ class Database:
             return str(char) + '\n\n{:d} stat points used.'.format(sum(char.statPoints.values()))
         return False
 
-    def updateHealth(self,serverId,charName,newHealth):
-        char = self.getCharacter(serverId,charName)
+    def updateHealth(self, serverId, charName, newHealth):
+        char = self.getCharacter(serverId, charName)
         char.health = newHealth
         return char
 
-    def respawnChar(self,serverId,charName):
-        char = self.getCharacter(serverId,charName)
+    def respawnChar(self, serverId, charName):
+        char = self.getCharacter(serverId, charName)
         char.respawn()
         return char
 
-    def updateLocation(self,serverId,charName,newX,newY):
-        char = self.getCharacter(serverId,charName)
-        char.pos = int(newX),int(newY)
+    def updateLocation(self, serverId, charName, newX, newY):
+        char = self.getCharacter(serverId, charName)
+        char.pos = int(newX), int(newY)
         return str(char)
 
     def updateSize(self, serverId, newX, newY):
@@ -103,7 +103,7 @@ class Database:
         battle.size = (newX, newY)
         return str(battle)
 
-    def insertAbility(self,serverId,codex,overwriteBattleLock=False):
+    def insertAbility(self, serverId, codex, overwriteBattleLock=False):
         battle = self.getBattle(serverId)
         char  = self.getCharacter(codex[0].lower())
         if char not in battle.participants or overwriteBattleLock:
@@ -117,7 +117,7 @@ class Database:
                 return str(abl)
         return False
 
-    def updateAbility(self,serverId,codex,overwriteBattleLock=False):
+    def updateAbility(self, serverId, codex, overwriteBattleLock=False):
         char = self.getCharacter(codex[0].lower())
         battle = self.getBattle(serverId)
         if char not in battle.participants or overwriteBattleLock:
@@ -126,51 +126,51 @@ class Database:
             return str(abl)
         return False
 
-    def getAbilities(self,serverId,charName):
-        char = self.getCharacter(serverId,charName)
+    def getAbilities(self, serverId, charName):
+        char = self.getCharacter(serverId, charName)
         return char.listAbilities()
 
-    def deleteChar(self,battleId,charName):
+    def deleteChar(self, battleId, charName):
         battle= self.getBattle(battleId)
         battle.deleteChar(charName)
 
-    def guildExists(self,guild):
+    def guildExists(self, guild):
         return guild.id in self.db
 
-    def getCurrentChar(self,serverId):
+    def getCurrentChar(self, serverId):
         battle = self.getBattle(serverId)
         return battle.currentChar()
 
-    def makeBattle(self,guild,battle):
+    def makeBattle(self, guild, battle):
          self.db[guild.id] = battle
 
-    def doPassTurn(self,serverId):
+    def doPassTurn(self, serverId):
         battle = self.getBattle(serverId)
         battle.passTurn()
         return battle.currentCharPretty()
 
-    def doBasicAttack(self,target,serverId):
+    def doBasicAttack(self, target, serverId):
         battle= self.getBattle(serverId)
         return battle.basicAttack(target) + '\n\n' + battle.currentCharPretty()
 
-    def doMove(self,codex,serverId):
+    def doMove(self, codex, serverId):
         battle = self.getBattle(serverId)
         return battle.move(codex) + '\n\n' + battle.currentCharPretty()
 
-    def doAbility(self,codex,serverId):
+    def doAbility(self, codex, serverId):
         battle = self.getBattle(serverId)
         return battle.useAbility(codex) + '\n\n' + battle.currentCharPretty()
 
-    def clearBattle(self,battleId):
+    def clearBattle(self, battleId):
         battle = self.getBattle(battleId)
         battle.clear()
 
-    def addParticipant(self,charName,battleId):
+    def addParticipant(self, charName, battleId):
         battle = self.getBattle(battleId)
         battle.addParticipant(charName)
         return charName + ' has successfully joined the battle!'
 
-    def getModifiers(self,serverId,charName):
-        char = self.getCharacter(serverId,charName)
+    def getModifiers(self, serverId, charName):
+        char = self.getCharacter(serverId, charName)
         return char.listModifiers()
 
